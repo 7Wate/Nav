536fd9d79d426b911161c1e0eb9339dddb2d973e
7Wate <zhouzhongping@7wate.com>
2022-11-17 16:41:56 +0800

    Project: init

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,6 @@
 var sindex = 0;
 var cycle = false;
-var sengine = "https://www.google.com/?q="; // Default search engine
+var sengine = "https://www.google.com/search?q="; // Default search engine
 
 function start() {
     var query = getParameterByName('q');
@@ -57,44 +57,26 @@ function search(text) {
     if (text[0] === '/') {
         if (text.indexOf(' ') > -1) {
             switch (option) {
-                case "am":
-                    window.location = "https://www.allmusic.com/search/all/" + subtext;
-                    break;
                 case "d":
                     window.location = "https://duckduckgo.com/?q=" + subtext;
                     break;
-                case "di":
-                    window.location = "https://www.discogs.com/search/?q=" + subtext;
+                case "b":
+                    window.location = "https://www.baidu.com/s?wd=" + subtext;
                     break;
-                case "i":
-                    window.location = "https://www.imdb.com/find?q=" + subtext;
-                    break;
-                case "m":
-                    window.location = "https://www.themoviedb.org/search?query=" + subtext;
-                    break;
-                case "r":
-                    window.location = "https://www.reddit.com/search?q=" + subtext;
-                    break;
-                case "q":
-                    window.location = "https://www.qwant.com/?q=" + subtext;
-                    break;
-                case "so":
-                    window.location = "https://soundcloud.com/search?q=" + subtext;
-                    break;
-                case "s":
-                    window.location = "https://open.spotify.com/search/results/" + subtext;
-                    break;
-                case "t":
-                    window.location = "https://trakt.tv/search?query=" + subtext;
-                    break;
-                case "tv":
-                    window.location = "https://www.thetvdb.com/search?query=" + subtext;
+                case "bk":
+                    window.location = "https://kaifa.baidu.com/searchPage?wd=" + subtext;
                     break;
                 case "y":
                     window.location = "https://www.youtube.com/results?search_query=" + subtext;
                     break;
                 case "g":
                     window.location = "https://www.google.com/?q=" + subtext;
+                    break;
+                case "gh":
+                    window.location = "https://github.com/search?q=" + subtext;
+                    break;
+                case "s":
+                    window.location = "https://stackoverflow.com/search?q=" + subtext;
                     break;
             }
         } else {
@@ -105,12 +87,6 @@ function search(text) {
                     break;
                 case "y":
                     window.location = "https://www.youtube.com";
-                    break;
-                case "r":
-                    window.location = "https://reddit.com";
-                    break;
-                case "s":
-                    window.location = "https://open.spotify.com";
                     break;
             }
         }
